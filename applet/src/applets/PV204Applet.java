03e34586415c93b2400f9f3c415615bf06e281d5
Nomit1993 <57198545+Nomit1993@users.noreply.github.com>
2020-04-21 19:39:08 +0200

    Update PV204Applet.java

--- applet/src/applets/PV204Applet.java
+++ applet/src/applets/PV204Applet.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 import javacard.framework.*;
 import javacard.security.*;
 import javacardx.crypto.Cipher;
+import static simpleapdu.SimpleAPDU.bitb;
+import static simpleapdu.SimpleAPDU.btbi;
 
 public class SimpleApplet extends javacard.framework.Applet 
 {
@@ -25,6 +27,7 @@ public class SimpleApplet extends javacard.framework.Applet
     byte[] baTempW = new byte[33];
     byte[] baTempS = new byte[17];
     byte[] baTempSS = new byte[17];
+    byte[] g = new byte[17];
     short lenA, lenB, lenP, lenW, lenS, lenSS;
     KeyPair kpU;
     ECPrivateKey privKeyU;
@@ -35,6 +38,8 @@ public class SimpleApplet extends javacard.framework.Applet
     private byte baPrivKeyU[] = new byte[17];
     private byte baPubKeyU[] = new byte[17];
     private byte baPubKeyV[] = new byte[17];
+    byte[] hashBuffer = JCSystem.makeTransientByteArray((short) 20, JCSystem.CLEAR_ON_RESET);
+
     
     protected SimpleApplet(byte[] buffer, short offset, byte length) {
         // TODO: Parse the supplied parameters.
@@ -102,7 +107,6 @@ public class SimpleApplet extends javacard.framework.Applet
             case (byte) 0xD1: process1(apdu); return;
             case (byte) 0xD2: process2(apdu); return;
             case (byte) 0xD3: process3(apdu); return;
-            case (byte) 0xD4: process4(apdu); return;
             default:    ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED); break;
         }
     }
@@ -118,14 +122,13 @@ public class SimpleApplet extends javacard.framework.Applet
     
     private void process1(APDU apdu)
     {
-        byte[] apduBuf1 = apdu.getBuffer();
-
+        byte[] apduBuf = apdu.getBuffer();
+       
         System.out.println("********************U parameters (Card Side)********************");
 
         byte pin[] = {0x31,0x32,0x33,0x34};
         System.out.print("PIN Set on Card: 1234");
         System.out.println();
-        byte[] hashBuffer = JCSystem.makeTransientByteArray((short) 20, JCSystem.CLEAR_ON_RESET);
         hash.doFinal(pin,(short)0,(short)pin.length,hashBuffer,(short)0);
         System.out.print("HASH OF PIN: ");
         for (byte b:hashBuffer) System.out.print(String.format("%X",b));
@@ -161,7 +164,12 @@ public class SimpleApplet extends javacard.framework.Applet
         System.out.print("Private Key (U) " + lenS + " :");
         for (byte b: baTempS) System.out.print(String.format("%02X", b));
         System.out.println();
-        
+    
+        baPubKeyV = Arrays.copyOfRange(apduBuf, 5, lenB + 5);
+        System.out.print("B Parameter Received from Host (V) " +lenB + " :");
+        for (byte b: baPubKeyV) System.out.print(String.format("%02X", b));
+        System.out.println();
+       
         //if(Arrays.equals(baTempSS, baTempSS1) == true)
         //start();        
         //G = Hash(PIN) mod P ---- DONE
@@ -169,18 +177,42 @@ public class SimpleApplet extends javacard.framework.Applet
         //V = (G ^ B) mod P
         //Hash(PIN) = hashBuffer
         
-        BigInteger p = new BigInteger(baTempP);
-        BigInteger G_number = OS2IP(hashBuffer).mod(p);
-        byte G_byte[] = I2OSP(G_number, 16);
-        System.out.print("Calculated G (U): ");
-        for(byte b:G_byte) System.out.print(String.format("%X",b));
-        System.out.println();
+        BigInteger p = btbi(baTempP);
         
-        Util.arrayCopyNonAtomic(baTempW, (short) 0, apduBuf1, ISO7816.OFFSET_CDATA, lenW);
-        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, lenW);
+        BigInteger g1 = btbi(hashBuffer).mod(p);
+        g = bitb(g1, 16);
+        
+        System.out.print("G (U): ");
+        for (byte b: g) System.out.print(String.format("%02X", b));
+        System.out.println();
+   
+        /*BigInteger a1 = btbi(baPubKeyV);
+        Random rand = new Random();
+        int a = rand.nextInt(1);
+        //System.out.println(a.intValue());
+        BigInteger midu = btbi(g).pow(a).mod(p);
+        byte[] midU = bitb(midu, 16);
+        
+        BigInteger bb = btbi(baTempA);
+        BigInteger k1 = btbi(midU).pow(bb.intValue()).mod(p);
+        byte[] k = bitb(k1, 16);
+        
+        System.out.print("Shared Secret At Card (U) " + k.length + " :");
+        for (byte b: k) System.out.print(String.format("%02X", b));
+        System.out.println();*/
+        
+        Util.arrayCopyNonAtomic(baTempA, (short) 0, apduBuf, ISO7816.OFFSET_CDATA, (short)lenA);
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short)lenA);
+    }
+        
+    private void process2(APDU apdu)
+    {
+        byte[] apduBuf = apdu.getBuffer();
+        Util.arrayCopyNonAtomic(g, (short) 0, apduBuf, ISO7816.OFFSET_CDATA, (short)g.length);
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short)g.length);
     }
     
-        // helper functions for SPEKE calculations [IEE163] [https://github.com/chetan51/ABBC/blob/master/src/main/java/RSAEngine/Crypter.java]
+    // helper functions for SPEKE calculations [IEE163] [https://github.com/chetan51/ABBC/blob/master/src/main/java/RSAEngine/Crypter.java]
     public static BigInteger OS2IP(byte[]X)
     {
         BigInteger out = new BigInteger("0");
@@ -215,63 +247,40 @@ public class SimpleApplet extends javacard.framework.Applet
 	//Cur is an array of two bigints, with cur[0]=X/256^(XLen-i) and cur[1]=X/256^[XLen-i]
         return out;
     }
-    
-    private void process2(APDU apdu)
-    {
-        byte[] apduBuf2 = apdu.getBuffer();
-
-        baPubKeyV = Arrays.copyOfRange(apduBuf2, 5, 38);
-        System.out.print("Public Key Received from Host (V) " +lenW + " :");
-        for (byte b: baPubKeyV) System.out.print(String.format("%02X", b));
-        System.out.println();
-       
-        Util.arrayCopyNonAtomic(baTempS, (short) 0, baPrivKeyU, (short) 0, (short) lenS);
-        
-        ecdhU = KeyAgreement.getInstance(KeyAgreement.ALG_EC_SVDP_DH, false);
-        ecdhU.init(privKeyU);
-        lenSS = ecdhU.generateSecret(baPubKeyV, (short)0, lenW, baTempSS, (short) 0);
-        System.out.print("Shared Secred U and V (U) " + lenSS + " :");
-        for (byte b: baTempSS) System.out.print(String.format("%02X", b));
-        System.out.println();
-        
-        Util.arrayCopyNonAtomic(baTempSS, (short) 0, apduBuf2, ISO7816.OFFSET_CDATA, (short) baTempSS.length);
-        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short) baTempSS.length);
-    }
         
     private void process3(APDU apdu)
     {
-        byte[] apduBuf3 = apdu.getBuffer();
-    }
-    
-    private void process4(APDU apdu) 
-    {
-        byte[] apduBuf4 = apdu.getBuffer();
-        byte[] sharedkey = new byte[16];
-        Util.arrayCopyNonAtomic(apduBuf4, (short)5, sharedkey, (short)0, (short)16);
-  
-        AESKey aesKeyTrial= (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
-        byte[] input = {(byte)0x44,(byte)0x22,(byte)0x33,(byte)0x44,(byte)0x55,(byte)0x66,(byte)0x77,(byte)0x88,(byte)0x99,0x10,(byte)0xA2, 0x35, (byte)0x5E,0x15,0x16,0x14};
+        byte[] apduBuf = apdu.getBuffer();
+        
+        byte[] input = Arrays.copyOfRange(apduBuf, 5, 21);
+        short len = (short)input.length;
         byte[] output = new byte[16];
-        short len = (short) input.length;
-
-        System.out.print("Input: ");
+        System.out.print("Received Encrypted Input from Host (U): ");
         for (byte b: input) System.out.print(String.format("%02X", b));
         System.out.println();
         
-        aesKeyTrial.setKey(baTempSS,(short)0);
+        AESKey aesKeyTrial= (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+        aesKeyTrial.setKey(g,(short)0);
         Cipher aesCipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
-        aesCipher.init(aesKeyTrial, Cipher.MODE_ENCRYPT);
-        aesCipher.doFinal(input, (short)0, len, output, (short)0); 
-        System.out.print("Output: ");
+        aesCipher.init(aesKeyTrial, Cipher.MODE_DECRYPT);
+        aesCipher.doFinal(input, (short)0, (short)len, output, (short)0);
+        System.out.print("Decrypted Input from Host (U): ");
         for (byte b: output) System.out.print(String.format("%02X", b));
         System.out.println();
         
-        byte[] input1 = new byte[16];
-        aesKeyTrial.setKey(baTempSS,(short)0);
-        aesCipher.init(aesKeyTrial, Cipher.MODE_DECRYPT);
-        aesCipher.doFinal(output, (short)0, len, input1, (short)0);
-        System.out.print("Input Once Again: ");
-        for (byte b: input1) System.out.print(String.format("%02X", b));
+        byte[] newinput = {(byte)0x43,(byte)0x22,(byte)0x33,(byte)0x44,(byte)0x55,(byte)0x66,(byte)0x77,(byte)0x88,(byte)0x99,0x10,(byte)0xA2, 0x35, (byte)0x5E,0x15,0x16,0x14};
+        System.out.print("New Input (U): ");
+        for (byte b: newinput) System.out.print(String.format("%02X", b));
         System.out.println();
-    }
+        byte[] output1 = new byte[16];
+        aesKeyTrial.setKey(g,(short)0);
+        aesCipher.init(aesKeyTrial, Cipher.MODE_ENCRYPT);
+        aesCipher.doFinal(newinput, (short)0, len, output1, (short)0); 
+        System.out.print("Encrypted Input (U): ");
+        for (byte b: output1) System.out.print(String.format("%02X", b));
+        System.out.println();
+        
+        Util.arrayCopyNonAtomic(output1, (short) 0, apduBuf, ISO7816.OFFSET_CDATA, (short)output1.length);
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short)output1.length);
+   }
 }
